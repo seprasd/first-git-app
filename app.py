303bde9a4843b201ed9e31ff89dc01818ce30c22
sepra <seprasd28@gmail.com>
2024-10-14 22:30:19 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("campusX")
+col1, col2 = st.columns(2)
+with col1:
+    st.image("unnamed.jpeg")
+with col2:
+    st.write(""" AT its core, CampusX aims to change the education system in INDIA""")
